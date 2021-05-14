114ad430709eb668a52c96df3cff1b9702fd95f1
vtchris <vtchris.2357@gmail.com>
2021-05-14 11:39:57 -0400

    102: Employee Interface

---
+++ Repositories/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManager.Azure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Azure.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        List<Employee> SelectAll();
+        Employee SelectByID(int id);
+        void Insert(Employee emp);
+        void Update(Employee emp);
+        void Delete(int id);
+        List<string> SelectCountries();
+    }
+}
